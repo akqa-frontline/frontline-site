67740c5d5ad5475fb83e9edbf85e25070955118b
Christine Linding Nielsen <christinelinding@akqa.com>
2021-01-07 11:26:23 +0100

    feat(layout): add title to pages

--- src/layouts/docs.tsx
+++ src/layouts/docs.tsx
@@ -4,18 +4,22 @@ import { WithSidesLayout } from '@/design-system/layouts/WithSides';
 import { SiteTopBar } from '@/components/navigation/SiteTopBar';
 import { DocsSidebar } from '@/components/navigation/DocsSidebar';
 import { DocsTableOfContents } from '@/components/navigation/DocsTableOfContents';
+import { Heading } from '@/design-system/components/elements/heading/Heading';
 
 /**
  * Helper Component used by next-mdx-enhanced
  */
-function Docs({ children, frontMatter: { tableOfContents } }) {
+function Docs({ children, frontMatter: { title, tableOfContents } }) {
   return (
     <WithSidesLayout
       left={<DocsSidebar />}
       top={<SiteTopBar />}
       right={<DocsTableOfContents tableOfContents={tableOfContents} />}
     >
-      {children}
+      <>
+        <Heading as="h1">{title}</Heading>
+        {children}
+      </>
     </WithSidesLayout>
   );
 }
