0e78004c84dde98637d55368eca79661cb1c0006
Christine Linding Nielsen <christinelinding@akqa.com>
2021-01-08 13:46:21 +0100

    docs(demo): update react example

--- src/components/playground/demo.tsx
+++ src/components/playground/demo.tsx
@@ -1,12 +1,26 @@
 import { Button } from '@/design-system/components/elements/button/Button';
 import React, { useState } from 'react';
 
+const DemoStyles = {
+  backgroundColor: 'dark',
+  '&:hover': {
+    backgroundColor: 'black',
+  },
+};
+
 export const Demo = () => {
   const [tapped, setTapped] = useState(false);
 
   return (
-    <Button onClick={() => setTapped(!tapped)}>
-      Tap this React Component {tapped ? 'Yes' : 'No'}
+    <Button
+      sx={{
+        ...(tapped && {
+          ...DemoStyles,
+        }),
+      }}
+      onClick={() => setTapped(!tapped)}
+    >
+      Tap this React Component
     </Button>
   );
 };
